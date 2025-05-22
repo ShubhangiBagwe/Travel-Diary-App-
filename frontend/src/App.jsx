b3156ce362f2b29a52eae47c0a7063312df689ed
Shubhangi Bagwe <shubhangibagwe@Shubhangi-Bagwes-MacBook-Pro.local>
2025-05-22 15:21:42 +0530

    react-router-dom and installation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import React from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home/Home"
+import Login from "./pages/Auth/Login"
+import SignUp from "./pages/Auth/SignUp"
+
 function App() {
   return (
     <>
-      <h1 className="text-5xl text-red-700">App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route  path="/sign-up" exact element={<SignUp/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
