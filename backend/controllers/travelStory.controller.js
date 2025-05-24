4e7592dddd3bd65bf1ba29f2d8c153882ffc3c03
ShubhangiBagwe <shubhangibagwe2000@gmail.com>
2025-05-24 23:39:08 +0530

    get all travel stories api routes

--- backend/controllers/travelStory.controller.js
+++ backend/controllers/travelStory.controller.js
@@ -26,10 +26,24 @@ export const addTravelStory = async (req, res, next) => {
         await travelStory.save()
 
         res.status(200).json({
-            story:travelStory,
-            message:"Story added successfully"
+            story: travelStory,
+            message: "Story added successfully"
         })
     } catch (err) {
         console.log(err)
+    }
+}
+
+export const getAllTravelStory = async (req, res, next) => {
+    const userId = req.user.id
+
+    try {
+        const travelStories = await TravelStory.find({ userId: userId }).sort({
+            isFavourite: -1,
+        })
+
+        res.status(200).json({ stories: travelStories })
+    } catch (error) {
+        console.log(error)
     }
 }
