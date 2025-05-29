ee2d7dc86a772e71c6c1371ab72a4396d4cde97a
ShubhangiBagwe <shubhangibagwe2000@gmail.com>
2025-05-29 12:22:43 +0530

    delete image api done

--- backend/controllers/travelStory.controller.js
+++ backend/controllers/travelStory.controller.js
@@ -3,6 +3,7 @@ import TravelStory from "../models/travelStory.model.js"
 import { errorHandler } from "../utils/error.js"
 import path from "path"
 import fs from "fs"
+import { response } from "express"
 
 export const addTravelStory = async (req, res, next) => {
   const { title, story, visitedLocation, imageUrl, visitedDate } = req.body
@@ -38,32 +39,68 @@ export const addTravelStory = async (req, res, next) => {
 }
 
 export const getAllTravelStory = async (req, res, next) => {
-    const userId = req.user.id
+  const userId = req.user.id
 
-    try {
-        const travelStories = await TravelStory.find({ userId: userId }).sort({
-            isFavourite: -1,
-        })
+  try {
+    const travelStories = await TravelStory.find({ userId: userId }).sort({
+      isFavourite: -1,
+    })
 
-        res.status(200).json({ stories: travelStories })
-    } catch (error) {
-        console.log(error)
-    }
+    res.status(200).json({ stories: travelStories })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 
 export const imageUpload = async (req, res, next) => {
-    try {
+  try {
 
-        if (!req.file) {
-            return next(errorHandler(400, "No image upladed"))
-        }
-
-        const imageUrl = `http://localhost:3000/uploads/${req.file.filename}`
-
-        res.status(201).json({ imageUrl })
-
-    } catch (error) {
-        next(error)
+    if (!req.file) {
+      return next(errorHandler(400, "No image upladed"))
     }
+
+    const imageUrl = `http://localhost:3000/uploads/${req.file.filename}`
+
+    res.status(201).json({ imageUrl })
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+const __fileName = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__fileName)
+
+const rootDir = path.join(__dirname, "..")
+
+export const deleteImage = async (req, res, next) => {
+  const { imageUrl } = req.query
+
+  if (!imageUrl) {
+    return next(errorHandler(400, "imageUrl parameter is required!"))
+  }
+
+  try {
+    const filename = path.basename(imageUrl)
+
+    const filePath = path.join(rootDir, "uploads", filename)
+
+    console.log(filePath)
+
+    // Check the file exist
+
+    if (!fs.existsSync(filePath)) {
+      return next(errorHandler(404, "Image not found"))
+    }
+
+    // delete the file
+
+    await fs.promises.unlink(filePath)
+
+    res.status(200).json({ message: "Image Deleted Successfully!" })
+  }
+  catch (error) {
+    next(error)
+  }
 }
